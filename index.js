aab25f53b52142cc7d3d31b973c21458a61ba12a
nsrksreecharan <75178502+nsrksreecharan@users.noreply.github.com>
2022-12-28 19:23:55 +0530

    cors error update

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path=require("path");
 const { ObjectId } = require("mongodb");
 const app=express();
 app.use((req, res, next) => { 
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3004');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
     next(); 
 }); 
