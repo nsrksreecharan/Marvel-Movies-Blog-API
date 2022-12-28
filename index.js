20ec496f091d190c8571014c65f59c4f06467ccd
nsrksreecharan <75178502+nsrksreecharan@users.noreply.github.com>
2022-12-28 22:15:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { ObjectId } = require("mongodb");
 const app=express();
 app.use((req, res, next) => { 
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,Authorization'); 
     next(); 
 }); 
 
