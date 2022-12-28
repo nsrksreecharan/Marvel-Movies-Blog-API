a99a0d3f75461ce8b3edfba9c8de5e994909cb80
nsrksreecharan <75178502+nsrksreecharan@users.noreply.github.com>
2022-12-28 19:36:17 +0530

    update headers to access controller

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const { ObjectId } = require("mongodb");
 const app=express();
 app.use((req, res, next) => { 
     res.header('Access-Control-Allow-Origin', 'http://localhost:3004');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); 
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept,authorization'); 
     next(); 
 }); 
 
